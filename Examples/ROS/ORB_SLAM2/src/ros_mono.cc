576a53eabb230507839dd215ce8923bd2e6df197
nopnop2002 <hau26280@tree.odn.ne.jp>
2023-02-06 17:08:13 +0900

    Add Transform conversion

--- Examples/ROS/ORB_SLAM2/src/ros_mono.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_mono.cc
@@ -27,6 +27,9 @@
 #include<ros/ros.h>
 #include <cv_bridge/cv_bridge.h>
 
+#include <tf2/convert.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+
 #include<opencv2/core/core.hpp>
 
 #include"../../../include/System.h"
@@ -45,6 +48,7 @@ public:
 
 int main(int argc, char **argv)
 {
+	printf("main start\n");
     ros::init(argc, argv, "Mono");
     ros::start();
 
@@ -58,12 +62,17 @@ int main(int argc, char **argv)
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
     ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
 
+	// bUseViewer = false
+    //ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,false);
+
     ImageGrabber igb(&SLAM);
 
     ros::NodeHandle nodeHandler;
     ros::Subscriber sub = nodeHandler.subscribe("/camera/image_raw", 1, &ImageGrabber::GrabImage,&igb);
 
+	printf("spin start\n");
     ros::spin();
+	printf("spin end\n");
 
     // Stop all threads
     SLAM.Shutdown();
@@ -75,6 +84,63 @@ int main(int argc, char **argv)
 
     return 0;
 }
+
+tf2::Transform TransformFromMat (cv::Mat position_mat) {
+  cv::Mat rotation(3,3,CV_32F);
+  cv::Mat translation(3,1,CV_32F);
+
+  rotation = position_mat.rowRange(0,3).colRange(0,3);
+  translation = position_mat.rowRange(0,3).col(3);
+
+
+  tf2::Matrix3x3 tf_camera_rotation (rotation.at<float> (0,0), rotation.at<float> (0,1), rotation.at<float> (0,2),
+                                    rotation.at<float> (1,0), rotation.at<float> (1,1), rotation.at<float> (1,2),
+                                    rotation.at<float> (2,0), rotation.at<float> (2,1), rotation.at<float> (2,2)
+                                   );
+
+  tf2::Vector3 tf_camera_translation (translation.at<float> (0), translation.at<float> (1), translation.at<float> (2));
+
+  //Coordinate transformation matrix from orb coordinate system to ros coordinate system
+  const tf2::Matrix3x3 tf_orb_to_ros (0, 0, 1,
+                                    -1, 0, 0,
+                                     0,-1, 0);
+
+  //Transform from orb coordinate system to ros coordinate system on camera coordinates
+  tf_camera_rotation = tf_orb_to_ros*tf_camera_rotation;
+  tf_camera_translation = tf_orb_to_ros*tf_camera_translation;
+
+  //Inverse matrix
+  tf_camera_rotation = tf_camera_rotation.transpose();
+  tf_camera_translation = -(tf_camera_rotation*tf_camera_translation);
+
+  //Transform from orb coordinate system to ros coordinate system on map coordinates
+  tf_camera_rotation = tf_orb_to_ros*tf_camera_rotation;
+  tf_camera_translation = tf_orb_to_ros*tf_camera_translation;
+
+  return tf2::Transform (tf_camera_rotation, tf_camera_translation);
+}
+
+geometry_msgs::Transform ConvertPositionToTransform (std_msgs::Header header, cv::Mat position) {
+	tf2::Transform tf_position = TransformFromMat(position);
+	//geometry_msgs::PoseStamped pose_msg;
+	geometry_msgs::Transform trans_msg;
+	//trans_msg.header = header;
+	trans_msg = tf2::toMsg(tf_position); 
+	//std::cout << "trans_msg=" << trans_msg << std::endl << std::endl;
+	return trans_msg;
+}
+
+geometry_msgs::TransformStamped ConvertPositionToTransformStamped (std_msgs::Header header, cv::Mat position, string child_frame_id) {
+	tf2::Transform tf_position = TransformFromMat(position);
+	//geometry_msgs::PoseStamped pose_msg;
+	geometry_msgs::TransformStamped trans_msg;
+	trans_msg.header = header;
+	trans_msg.transform = tf2::toMsg(tf_position); 
+	trans_msg.child_frame_id = child_frame_id;
+	//std::cout << "trans_msg=" << trans_msg << std::endl << std::endl;
+	return trans_msg;
+}
+
 
 void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
 {
@@ -90,7 +156,26 @@ void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
         return;
     }
 
-    mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
+    // Proccess the given monocular frame
+    // Input images: RGB (CV_8UC3) or grayscale (CV_8U). RGB is converted to grayscale.
+    // Returns the camera pose (empty if tracking fails).
+	// cv::Mat TrackMonocular(const cv::Mat &im, const double &timestamp);
+	printf("TrackMonocular timestamp=%f\n", cv_ptr->header.stamp.toSec());
+    //mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
+    cv::Mat m = mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
+	// camera pose として 4x4 の配列が戻る
+	std::cout << "m=" << m << std::endl << std::endl;
+	if (!m.empty()) {
+		// Convert to geometry_msgs::Transform
+		geometry_msgs::Transform transform;
+		transform = ConvertPositionToTransform(cv_ptr->header, m);
+		std::cout << "transform=" << transform << std::endl << std::endl;
+
+		// Convert to geometry_msgs::TransformStamped
+		geometry_msgs::TransformStamped transformStamped;
+		transformStamped = ConvertPositionToTransformStamped(cv_ptr->header, m, "/child");
+		std::cout << "transformStamped=" << transformStamped << std::endl << std::endl;
+	}
 }
 
 
